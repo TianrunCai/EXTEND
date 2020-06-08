b92b0ea7b080080865fe8b574a74c8753eb87234
Tianrun Cai <trcaisj2@gmail.com>
2020-06-08 15:36:23 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  8 15:12:45 2020
+
+@author: AustinMain
+"""
+
+import site
+import os
+import shutil
+
+packagepath = site.getsitepackages()[1]
+
+def run():
+    targetfolder = os.path.join(packagepath,'ExtEND')
+    try:
+        os.mkdir(targetfolder)
+    except:
+        print (packagepath, 'does not exist!')
+        
+    for root, dirs, files in os.walk('EXTEND'):
+        print (files)
+        print (root)
+        for f in files:
+            shutil.copy(os.path.join(root,f), os.path.join(targetfolder,f))
+            
+if __name__ == '__main__':
+    run()
